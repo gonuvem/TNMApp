13cfde894ccaa7fd722829c575c44c373bdaf00f
Francisco Mendes <fmendes_cn@hotmail.com>
2020-08-27 16:12:23 -0300

    Add roboto font on project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>TnmAplicativo</Text>
+    </View>
+  );
+};
+
+export default App;
