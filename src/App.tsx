7fc1f6340a348ee8a89c56b4a157dd0c9d9eaae8
Francisco Mendes <fmendes_cn@hotmail.com>
2020-09-17 16:20:03 -0300

    Add firebase admob

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar} from 'react-native';
+import admob, {MaxAdContentRating} from '@react-native-firebase/admob';
 
 import {colors} from './general/colors';
 import Router from './router';
 
 const App: React.FC = () => {
+  // useEffect(() => {
+  //   admob()
+  //     .setRequestConfiguration({
+  //       // Update all future requests suitable for parental guidance
+  //       maxAdContentRating: MaxAdContentRating.PG,
+
+  //       // Indicates that you want your content treated as child-directed for purposes of COPPA.
+  //       tagForChildDirectedTreatment: true,
+
+  //       // Indicates that you want the ad request to be handled in a
+  //       // manner suitable for users under the age of consent.
+  //       tagForUnderAgeOfConsent: true,
+  //     })
+  //     .then(() => {
+  //       // Request config successfully set!
+  //     });
+  // }, []);
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor={colors.primary} />
--- src/components/AdMob/index.tsx
+++ src/components/AdMob/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import {BannerAd, TestIds, BannerAdSize} from '@react-native-firebase/admob';
 
-import { Container } from './styles';
+import {Container} from './styles';
 
-const AbMob: React.FC = () => <Container />
+const adUnitId = __DEV__
+  ? TestIds.BANNER
+  : 'ca-app-pub-3163868750666019~9703837345';
+
+const AbMob: React.FC = () => (
+  <Container>
+    <BannerAd unitId={adUnitId} size={BannerAdSize.SMART_BANNER} />
+  </Container>
+);
 
 export default AbMob;
